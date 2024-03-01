86bc38f424a6b7e9df2f274c972c2749525b1102
DulhanPerera <vicksura.20210165@iit.ac.lk>
2024-03-01 11:26:05 +0530

    Adding updates on constructors

--- src/main/java/com/mycompany/ooprevise/Constructors.java
+++ src/main/java/com/mycompany/ooprevise/Constructors.java
@@ -9,5 +9,25 @@ package com.mycompany.ooprevise;
  * @author Dulhan
  */
 public class Constructors {
+    public static void main(String[] args) {
+        
+        Human human1 = new Human("Dulhan", 22, 100.00);
+        Human human2 = new Human("Risindu", 18, 110.00);
+        
+        System.out.println(human1.name);
+        System.out.println(human1.age);
+        System.out.println(human1.weight);
+        
+        System.out.println();
+        
+        System.out.println(human2.name);
+        System.out.println(human2.age);
+        System.out.println(human2.weight);
+        
+        System.out.println();
+        
+        human2.eat();
+        human1.drink();
+    }
     
 }
