9883e8be94ddb2757ff6e8ffd1159ef9426bf669
DulhanPerera <vicksura.20210165@iit.ac.lk>
2024-03-07 16:18:31 +0530

    Adding files that uses super keyword

---
+++ src/main/java/com/mycompany/ooprevise/Person.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mycompany.ooprevise;
+
+/**
+ * 
+ */
+public class Person {
+
+	String name;
+	int age;
+	
+	Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String toString() {
+		return this.name+"\n"+this.age;
+	}
+}
---
+++ src/main/java/com/mycompany/ooprevise/SuperKeyword.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.mycompany.ooprevise;
+
+/**
+ * 
+ */
+public class SuperKeyword {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Super = keyword refers to the superclass (parent) of a -n object very similar to the 'this' keyword.
+
+		Hero hero1 = new Hero("Batman", 40, "Money");
+		Hero hero2 = new Hero("Superman", 40, "Everything");
+		
+		hero2.toString();
+		System.out.println(hero2.toString());
+	}
+
+}
