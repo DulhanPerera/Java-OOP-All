ad803cb2cf7ba38ec0092ea358bffab1e6df8ce8
DulhanPerera <vicksura.20210165@iit.ac.lk>
2024-03-07 13:45:03 +0530

    New Files

---
+++ bin/README.md
@@ -0,0 +1,7 @@
+# Java Object Oriented Programming
+## Topics covered in this repo
+1. Objects
+2. Methods
+3. Method Overloading
+4. Constructors
+5. 
---
+++ src/main/java/com/mycompany/ooprevise/StaticKeyword.java
@@ -0,0 +1,14 @@
+package com.mycompany.ooprevise;
+
+/**
+ *
+ * @author Dulhan
+ */
+public class StaticKeyword {
+    public static void main(String[] args){
+        // Static - modifier, A single copy of a variable/ method is created and shared.
+        //          the class "owns" the static member.
+    	
+    	System.out.println("Hello");
+    }
+}
